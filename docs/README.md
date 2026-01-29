0e85970ba90122b6022bbc2a8497c692832b2a8a
Chloe Low <chloryfish@gmail.com>
2026-01-30 07:03:12 +0800

    Level 0

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# Crumb User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/Crumb.java
@@ -0,0 +1,10 @@
+public class Crumb {
+    public static void main(String[] args) {
+        String lineBreak = "-------------------------------------------------";
+        System.out.println(lineBreak);
+        System.out.println("Hey, I'm Crumb! ദ്ദി◝ ⩊ ◜)\nWhat can I do for you?");
+        System.out.println(lineBreak);
+        System.out.println("Bye! ヾ(￣▽￣)");
+    }
+
+}
