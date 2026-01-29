9f7cf75cbf463095c3250dcba1a14b6ae2a5758e
Chloe Low <chloryfish@gmail.com>
2026-01-30 07:11:55 +0800

    Level 1 - Echo

--- src/main/java/Crumb.java
+++ src/main/java/Crumb.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Crumb {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         String lineBreak = "-------------------------------------------------";
         System.out.println(lineBreak);
         System.out.println("Hey, I'm Crumb! ദ്ദി◝ ⩊ ◜)\nWhat can I do for you?");
         System.out.println(lineBreak);
+
+        String in = sc.nextLine();
+        while (!in.toLowerCase().equals("bye")) {
+            System.out.println(">> " + in + "\n");
+            in = sc.nextLine();
+        }
         System.out.println("Bye! ヾ(￣▽￣)");
     }
 
